47dd3a503448e34588f78f4468728cc054caa45d
Simon Maresz <simon@salsitasoft.com>
2017-01-05 13:04:02 +0100

    RELATED: NUI-62: reworked getLogs

--- src/admin/logs.js
+++ src/admin/logs.js
@@ -1,22 +1,10 @@
 import { get } from '../xhr';
 import * as routes from './routes';
 
-export const getSyncLog = (contractId, dataProductId, domainId, segmentId) =>
-    get(routes.interpolate(routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_SYNCLOG, {
+export const getLogs = (contractId, dataProductId, domainId, segmentId) =>
+    get(routes.interpolate(routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_LOG, {
         contractId,
         dataProductId,
         domainId,
         segmentId
-    })).then(data => ({
-        log: data.log.log
-    }));
-
-export const getChangeLog = (contractId, dataProductId, domainId, segmentId) =>
-    get(routes.interpolate(routes.CONTRACT_DATA_PRODUCT_DOMAIN_SEGMENTS_DOMAIN_CHANGELOG, {
-        contractId,
-        dataProductId,
-        domainId,
-        segmentId
-    })).then(data => ({
-        log: data.log.log
-    }));
+    })).then(data => data.logs.map(item => item.log));
